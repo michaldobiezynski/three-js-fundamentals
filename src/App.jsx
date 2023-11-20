42bade82a5e28f25abdb5b3ce03fcc38ec0ad459
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-20 11:16:05 +0000

    more playing with meshes

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,21 @@
 import { Canvas } from "@react-three/fiber";
 
+import * as THREE from "three";
+
 function App() {
   return (
-    <Canvas camera={{ position: [0, 3, 3] }}>
-      <mesh>
-        {/* <boxGeometry args={[1, 1, 1]} /> */}
-        <sphereGeometry args={[1, 32, 32]} />
-        {/* <meshBasicMaterial color={0x00ff00} /> */}
-        <meshNormalMaterial />
+    <Canvas camera={{ position: [3, 3, 3] }}>
+      <mesh position-x={-0.6}>
+        <boxGeometry />
+        <meshStandardMaterial color="hotpink" side={THREE.FrontSide} />
       </mesh>
+      <mesh position-x={0.6} position-z={-1}>
+        <boxGeometry />
+        <meshStandardMaterial color="hotpink" side={THREE.DoubleSide} />
+      </mesh>
+      <ambientLight intensity={0.5} />
+      <directionalLight position={[0, 0, 3]} intensity={1} />
+      <directionalLight position={[0, 3, 3]} intensity={0.5} />
     </Canvas>
   );
 }
