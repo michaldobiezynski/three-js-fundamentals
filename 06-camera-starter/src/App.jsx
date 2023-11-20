be1756b1977076e2638e54bbc5f52c97a87f8c76
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-20 11:46:02 +0000

    play around with PerspectiveCamera and OrthographicCamera cameras

--- 06-camera-starter/src/App.jsx
+++ 06-camera-starter/src/App.jsx
@@ -1,10 +1,25 @@
-import { Environment } from "@react-three/drei";
+import {
+  Environment,
+  OrbitControls,
+  OrthographicCamera,
+  PerspectiveCamera,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { City } from "./components/City";
 
 function App() {
   return (
-    <Canvas camera={{ position: [3, 3, 3] }}>
+    <Canvas>
+      <OrthographicCamera
+        position={[1, 1, 1]}
+        makeDefault
+        top={2}
+        bottom={-2}
+        left={-2 * (window.innerWidth / window.innerHeight)}
+        right={2 * (window.innerWidth / window.innerHeight)}
+        near={-5}
+      />
+      <OrbitControls />
       {/* IGNORE FOR NOW */}
       <City />
       <Environment preset="city" />
