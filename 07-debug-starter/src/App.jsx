6b2389d573083ba893e5b2bbd9e206286109b3f1
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-20 11:54:01 +0000

    add grid from react three drei

---
+++ 07-debug-starter/src/App.jsx
@@ -0,0 +1,37 @@
+import { Canvas } from "@react-three/fiber";
+
+import { Grid } from "@react-three/drei";
+
+const Box = () => {
+  return (
+    <mesh>
+      <boxGeometry />
+      <meshBasicMaterial color="white" transparent opacity={0} />
+    </mesh>
+  );
+};
+
+function App() {
+  return (
+    <>
+      <Canvas camera={{ position: [3, 3, 3] }}>
+        <axesHelper />
+        {/* <gridHelper args={[10, 10, "green", "blue"]} /> */}
+        <Grid
+          sectionSize={3}
+          sectionColor={"purple"}
+          sectionThickness={1}
+          cellSize={1}
+          cellColor={"#6f6f6f"}
+          cellThickness={0.6}
+          infiniteGrid
+          fadeDistance={50}
+          fadeStrength={5}
+        />
+        <Box />
+      </Canvas>
+    </>
+  );
+}
+
+export default App;
