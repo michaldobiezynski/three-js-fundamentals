9e019d177dd5da67cba924c74c2c5cc74a05ef74
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-20 11:27:03 +0000

    rotate the cubes

--- 05-transforms-starter/src/App.jsx
+++ 05-transforms-starter/src/App.jsx
@@ -1,5 +1,7 @@
 import { Canvas } from "@react-three/fiber";
 
+import * as THREE from "three";
+
 function App() {
   return (
     <Canvas camera={{ position: [0, 3, 8] }}>
@@ -8,15 +10,22 @@ function App() {
       <directionalLight position={[0, 3, 5]} intensity={0.5} />
 
       {/* Objects */}
-      <mesh position={[-3, 0, 0]}>
+      <mesh
+        position-x={-3}
+        scale={[1, 0.5, 0.5]}
+        rotation={[0, THREE.MathUtils.degToRad(30), 0]}>
         <boxGeometry />
         <meshStandardMaterial color="red" />
       </mesh>
-      <mesh position={[0, 0, 0]}>
+      <mesh rotation-y={Math.PI / 4}>
         <boxGeometry />
         <meshStandardMaterial color="blue" />
       </mesh>
-      <mesh position={[3, 0, 0]}>
+      <mesh
+        position-x={3}
+        scale={2}
+        rotation-y={Math.PI / 4}
+        rotation-z={Math.PI / 4}>
         <boxGeometry />
         <meshStandardMaterial color="green" />
       </mesh>
