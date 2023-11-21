d1690e603a50aeb1c99adce87e2cc14b94a24d76
michaldobiezynski <michaldobiezynski@hotmail.com>
2023-11-21 20:52:44 +0000

    play around with different shadows

--- 09-shadows-starter/src/App.jsx
+++ 09-shadows-starter/src/App.jsx
@@ -1,24 +1,117 @@
-import { OrbitControls } from "@react-three/drei";
+import {
+  AccumulativeShadows,
+  BakeShadows,
+  ContactShadows,
+  OrbitControls,
+  RandomizedLight,
+  SoftShadows,
+} from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { useControls } from "leva";
 
 function App() {
+  const { cubeInAir } = useControls({
+    cubeInAir: false,
+  });
+
   return (
     <>
-      <Canvas camera={{ position: [0, 3, 3] }}>
+      <Canvas camera={{ position: [0, 3, 3] }} shadows>
+        <AccumulativeShadows
+          temporal
+          frames={35}
+          alphaTest={0.85}
+          scale={5}
+          position={[0, -0.499, 0]}
+          color={"#efbd4e"}>
+          <RandomizedLight
+            amount={4}
+            radius={9}
+            intensity={0.55}
+            ambient={0.25}
+            position={[5, 5, -10]}
+          />
+          <RandomizedLight
+            amount={4}
+            radius={9}
+            intensity={0.25}
+            ambient={0.55}
+            position={[-55, 5, -9]}
+          />
+        </AccumulativeShadows>
+
+        {/* <ContactShadows
+          frames={1}
+          position-y={-0.45}
+          opacity={0.5}
+          blur={2}
+          color={"pink"}
+          scale={10}
+        /> */}
+        {/* <SoftShadows />
+        <BakeShadows /> */}
         <OrbitControls />
         <ambientLight intensity={0.5} />
-        <directionalLight position={[5, 5, 5]} intensity={0.5} />
-        <directionalLight position={[-5, 5, 5]} intensity={0.5} color="red" />
+        <directionalLight
+          position={[5, 5, 5]}
+          intensity={0.5}
+          castShadow
+          // shadow-camera-far={50}
+          // shadow-camera-left={-10}
+          // shadow-camera-right={10}
+          // shadow-camera-top={10}
+          // shadow-camera-bottom={-10}
 
-        <mesh rotation-y={Math.PI / 4}>
+          // shadow-mapSize={[64, 64]}
+        >
+          {/* <orthographicCamera
+          
+            attach={"shadow-camera"}
+            args={[-10, 10, 10, -10]}
+          /> */}
+        </directionalLight>
+        <directionalLight
+          position={[-5, 5, 5]}
+          intensity={0.5}
+          color="red"
+          castShadow
+        />
+
+        <mesh position={[1, 1, 1]} castShadow>
+          <sphereGeometry args={[0.5, 32, 32]} />
+          <meshStandardMaterial color="white" />
+        </mesh>
+
+        <mesh
+          rotation-y={Math.PI / 4}
+          castShadow
+          receiveShadow
+          position-y={cubeInAir ? 1 : 0}>
           <boxGeometry />
           <meshStandardMaterial color="white" />
         </mesh>
 
-        <mesh rotation-x={-Math.PI / 2} position-y={-0.5}>
+        <mesh rotation-x={-Math.PI / 2} position-y={-0.5} receiveShadow>
           <planeGeometry args={[5, 5]} />
           <meshStandardMaterial color="white" />
         </mesh>
+
+        {/* <group position-x={10}>
+          <mesh position={[1, 1, 1]} castShadow>
+            <sphereGeometry args={[0.5, 32, 32]} />
+            <meshStandardMaterial color="white" />
+          </mesh>
+
+          <mesh rotation-y={Math.PI / 4} castShadow receiveShadow>
+            <boxGeometry />
+            <meshStandardMaterial color="white" />
+          </mesh>
+
+          <mesh rotation-x={-Math.PI / 2} position-y={-0.5} receiveShadow>
+            <planeGeometry args={[5, 5]} />
+            <meshStandardMaterial color="white" />
+          </mesh>
+        </group> */}
       </Canvas>
     </>
   );
